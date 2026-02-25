8906ffc70c0bd1f0b28c17a58dfd4fda01cfe8e7
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-25 21:31:15 +0530

    Duplicate name

--- pyspark-etl-project/infra/output.tf
+++ pyspark-etl-project/infra/output.tf
@@ -1,4 +1,7 @@
-output "ec2_public_ip" { value = aws_instance.pyspark_ec2.public_ip }
+output "public_ec2_public_ip" {
+  description = "Public IP of the PySpark EC2 instance"
+  value       = aws_instance.public_ec2.public_ip
+}
 output "rds_endpoint"    { value = aws_db_instance.mysql_rds.endpoint }
 #output "s3_bucket"       { value = aws_s3_bucket.csv_bucket.bucket }
 
