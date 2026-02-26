ac64f59f39967627eec0a6be2bf1bac14ed60829
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-26 15:28:24 +0530

    Requirements execution missing

--- pyspark-etl-project/etl/run_etl.sh
+++ pyspark-etl-project/etl/run_etl.sh
@@ -7,6 +7,8 @@ set -e
 PROJECT_DIR=~/pyspark-etl-project/etl
 ETL_SCRIPT="$PROJECT_DIR/src/load_s3_to_rds.py"
 LOG_DIR="$PROJECT_DIR/logs"
+REQUIREMENTS_FILE="$PROJECT_DIR/requirements.txt"
+
 mkdir -p "$LOG_DIR"
 
 # -----------------------------
@@ -19,9 +21,22 @@ echo "Starting ETL job at $(date)..."
 echo "Logs: $LOG_FILE"
 
 # -----------------------------
+# Install Python dependencies
+# -----------------------------
+echo "Installing Python dependencies..."
+sudo apt update -y && sudo apt install -y python3-pip
+pip3 install --upgrade pip
+
+if [ -f "$REQUIREMENTS_FILE" ]; then
+    pip3 install -r "$REQUIREMENTS_FILE"
+else
+    echo "⚠️  requirements.txt not found. Skipping dependency installation."
+fi
+
+# -----------------------------
 # Run Python ETL
 # -----------------------------
-python3 "$ETL_SCRIPT" > "$LOG_FILE" 2>&1
+python3 "$ETL_SCRIPT" > >(tee -a "$LOG_FILE") 2>&1
 
 EXIT_CODE=$?
 if [ $EXIT_CODE -eq 0 ]; then
