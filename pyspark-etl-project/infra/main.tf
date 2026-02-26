540400babff622b2984b7637fb32a4f51da0e2e8
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-26 11:46:57 +0530

    Issue fixes

--- pyspark-etl-project/infra/main.tf
+++ pyspark-etl-project/infra/main.tf
@@ -129,7 +129,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]   # Your public IP only
+    cidr_blocks = ["0.0.0.0/0"]   # Your public IP only
   }
 
   egress {
